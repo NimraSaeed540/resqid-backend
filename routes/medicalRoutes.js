65ef6258a59a995f61237b3e2a3527ed85aea84d
NimraSaeed540 <nimraminetech@gmail.com>
2026-02-25 00:43:08 +0500

    Fixed medicalRoutes import case issue

--- routes/medicalRoutes.js
+++ routes/medicalRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const MedicalProfile = require("../models/medicalprofile");
+const MedicalProfile = require("./models/medicalprofile");
 const protect = require("../middleware/authmiddleware");
 
 // ==============================
